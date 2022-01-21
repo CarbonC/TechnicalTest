651e104d5e4d03d93fff751e19f52905282ef6c4
Steven ZHENG <steven.zheng@epitech.eu>
2022-01-21 19:22:44 +0100

    [ADD] Metal groups visualization with a datagrid  of the csv file, can sort each column width different filters, the default sorting is not set

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import DataGridMetal from './components/DataGridMetal';
+
+function App() {
+  return (
+    <div className="App">
+      <DataGridMetal/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/DataGridMetal.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import './dataGridMetal.css'
+import { DataGrid } from '@mui/x-data-grid';
+import Papa from 'papaparse';
+
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'band_name', headerName: 'Band Name', width: 150},
+    { field: 'fans', headerName: 'Fans', width: 150},
+    { field: 'formed', headerName: 'Formed', width: 150},
+    { field: 'origin', headerName: 'Origin', width: 150 },
+    { field: 'split', headerName: 'Split', width: 150 },
+    { field: 'style', headerName: 'Style', resizable: true, width: 450},
+];
+
+async function getBandDataFromCSV() {
+    const response = await fetch('./metal_bands_2017.csv')
+    const reader = response.body.getReader();
+    const result = await reader.read();
+    const decoder = new TextDecoder('utf-8');
+    const csv = await decoder.decode(result.value);
+    const dataParsed = Papa.parse(csv);
+
+    return dataParsed.data.slice(1);
+}
+
+export default function DataGridMetal() {
+    var [rows, setRows] = React.useState([]);
+
+    React.useEffect(() => {
+        getBandDataFromCSV()
+        .then(data => {
+            data.map((object) => {
+                setRows(rows => [...rows, {
+                    id: object[0],
+                    band_name: object[1],
+                    fans: object[2],
+                    formed: object[3],
+                    origin: object[4],
+                    split: object[5],
+                    style: object[6]
+                }]);
+            })
+        })
+        .catch(err => console.log(err));
+    }, [])
+
+    return (
+        <div>
+            <div className="datagrid">
+                <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={10}
+                rowsPerPageOptions={[5]}
+                />
+            </div>
+        </div>
+    )
+}
