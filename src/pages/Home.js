54dce25429ccc17ef32d457145fe72bb07f650c5
Juliette Engel <juliette.engel@skema.edu>
2022-11-16 13:05:04 +0100

    add rooting bootstrap and home

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+const Home = () => {
+  return (
+    <Container>
+      <div>
+        <h1>Metal Bands</h1>
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
