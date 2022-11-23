0792b66ca67c824d221e9941cc02c2c816258409
NicolasOepitech <113346605+NicolasOepitech@users.noreply.github.com>
2022-11-23 17:34:19 +0100

    reorganized

--- front/src/App.js
+++ front/src/App.js
@@ -1,30 +1,15 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import "./App.css";
 
-import Table from "./Components/Table/table";
+import Table from "./Components/Table/Table";
 
 export default function App() {
-  const [bands, setBands] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:3500/bands")
-      .then((response) => {
-        console.log(response.data);
-        setBands(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
   return (
     <div className="App">
       <header>
         <h1>Metal Bands</h1>
+        <Table />
       </header>
-      <Table bands={bands} />
     </div>
   );
 }
--- front/src/Components/Table/table.js
+++ front/src/Components/Table/table.js
@@ -1,37 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function table(props) {
-  const { bands } = props;
+export default function Table(props) {
+  const [bands, setBands] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3500/bands")
+      .then((response) => {
+        console.log(response.data);
+        setBands(
+          response.data.sort((a, b) => a.band_name.localeCompare(b.band_name))
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>
-            <button>Band Name</button>
-          </th>
-          <th>Fans</th>
-          <th>Formed</th>
-          <th>
-            <button>Origin</button>
-          </th>
-          <th>Split</th>
-          <th>
-            <button>Style</button>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {bands.map((band) => (
-          <tr key={band._id}>
-            <td>{band.band_name}</td>
-            <td>{band.fans}</td>
-            <td>{band.formed}</td>
-            <td>{band.origin}</td>
-            <td>{band.split}</td>
-            <td>{band.style}</td>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <button>Band Name</button>
+            </th>
+            <th>Fans</th>
+            <th>Formed</th>
+            <th>
+              <button>Origin</button>
+            </th>
+            <th>Split</th>
+            <th>
+              <button>Style</button>
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {bands.map((band) => (
+            <tr key={band._id}>
+              <td>{band.band_name}</td>
+              <td>{band.fans}</td>
+              <td>{band.formed}</td>
+              <td>{band.origin}</td>
+              <td>{band.split}</td>
+              <td>{band.style}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
