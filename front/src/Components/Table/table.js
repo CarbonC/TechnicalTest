f8852f20d4af9130906f858a80b96c17c146d6f9
NicolasOepitech <113346605+NicolasOepitech@users.noreply.github.com>
2022-11-23 17:05:21 +0100

    All in table.js

--- front/src/Components/Table/table.js
+++ front/src/Components/Table/table.js
@@ -1,14 +1,37 @@
-import React from "react";
-import TableBody from "./tableBody";
-import TableHead from "./tableHead";
+import React, { useState } from "react";
 
 export default function table(props) {
+  const { bands } = props;
   return (
-    <div>
-      <table>
-        <TableHead />
-        <TableBody bands={props.bands} />
-      </table>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>
+            <button>Band Name</button>
+          </th>
+          <th>Fans</th>
+          <th>Formed</th>
+          <th>
+            <button>Origin</button>
+          </th>
+          <th>Split</th>
+          <th>
+            <button>Style</button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {bands.map((band) => (
+          <tr key={band._id}>
+            <td>{band.band_name}</td>
+            <td>{band.fans}</td>
+            <td>{band.formed}</td>
+            <td>{band.origin}</td>
+            <td>{band.split}</td>
+            <td>{band.style}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
