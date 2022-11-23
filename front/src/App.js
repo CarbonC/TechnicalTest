8251d73cae6d2137cd649fb55dad5e1bbf451c25
NicolasOepitech <113346605+NicolasOepitech@users.noreply.github.com>
2022-11-23 16:13:01 +0100

    fetch data with axios

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 export default function App() {
+  const [bands, setBands] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3500/bands").then((res) => {
+      setBands(res.data);
+    });
+  }, []);
+
   return (
-    <div className='App'>
+    <div className="App">
       <header>
         <h1>App</h1>
       </header>
     </div>
-  )
+  );
 }
